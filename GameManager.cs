7cc687a954d4d2cb668b915ff046b9968bbbfa19
unknown <melnic.andry97@gmail.com>
2018-08-29 14:48:50 +0200

    Commit 1

---
+++ GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TetrisGame
+{
+    class GameManager
+    {
+        #region  Attributes
+        private static Stopwatch timer;
+        private static Stopwatch dropTimer;
+        private static Stopwatch inputTimer;
+        private static int dropTime;
+        private static int dropRate;
+        private static int points;
+        private static int linesCleared;
+        private static int level;
+        #endregion
+
+        #region Constructor
+        public GameManager()
+        {
+            timer = new Stopwatch();
+            dropTimer = new Stopwatch();
+            inputTimer = new Stopwatch();
+            points = 0;
+            dropTime = 300;
+            dropRate = 300;
+            linesCleared = 0;
+            level = 1;
+
+        }
+        #endregion
+
+        #region Methods
+
+
+
+        #endregion
+    }
+}
---
+++ Grid.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TetrisGame
+{
+    class Grid
+    {
+        private static int[,] grid = new int[20, 10];
+        /*public static void fillGrid()
+        {
+            for (int i = 0; i < 23; i++)
+            {
+                Console.SetCursorPosition(1, i);
+                for(int j = 0; j < 10; j++)
+                {
+                    Console.Write(sqr);
+                }
+                Console.WriteLine();
+            }
+        }*/
+        public void drawBorder()
+        {
+            for (int lengthCount = 0; lengthCount <= 19; ++lengthCount)
+            {
+                Console.SetCursorPosition(0, lengthCount);
+                Console.Write("*");
+                Console.SetCursorPosition(18, lengthCount);
+                Console.Write("*");
+            }
+            Console.SetCursorPosition(0, 20);
+            for (int widthCount = 0; widthCount <= 10; widthCount++)
+            {
+                Console.Write("*-");
+            }
+        }
+    }
+}
